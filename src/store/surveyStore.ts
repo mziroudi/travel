4ca545aab0b9f8f1bd74be8bd176919e85e85413
mziroudi <mouad.ziroudi@gmail.com>
2025-02-23 20:37:27 +0000

    fix: Add component property to initialSteps in surveyStore

--- src/store/surveyStore.ts
+++ src/store/surveyStore.ts
@@ -1,5 +1,9 @@
 import { create } from 'zustand'
 import { SurveyState, SurveyData } from '@/types/survey'
+import { TravelDatesStep } from '@/components/survey/steps/TravelDatesStep'
+import { BudgetStep } from '@/components/survey/steps/BudgetStep'
+import { PreferencesStep } from '@/components/survey/steps/PreferencesStep'
+import { GroupDetailsStep } from '@/components/survey/steps/GroupDetailsStep'
 
 const initialSteps = [
   {
@@ -7,24 +11,28 @@ const initialSteps = [
     title: 'Travel Dates',
     description: 'When would you like to travel?',
     isComplete: false,
+    component: TravelDatesStep
   },
   {
     id: 'budget',
     title: 'Budget',
     description: 'What\'s your budget range?',
     isComplete: false,
+    component: BudgetStep
   },
   {
     id: 'preferences',
     title: 'Preferences',
     description: 'Tell us about your travel style and preferences',
     isComplete: false,
+    component: PreferencesStep
   },
   {
     id: 'group',
     title: 'Group Details',
     description: 'Who will be traveling with you?',
     isComplete: false,
+    component: GroupDetailsStep
   },
 ]
 
